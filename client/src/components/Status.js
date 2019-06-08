92acf4afe570c1aae5cf7115464567803d5a7f46
George701 <chernikov.georgii@gmail.com>
2019-06-08 20:25:44 +0300

    Update currect status

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -5,7 +5,8 @@ import PropTypes from "prop-types";
 
 class Status extends Component {
     state = {
-        id: ""
+        id: "",
+        status: ""
     }
     
     componentDidMount(){
@@ -13,22 +14,36 @@ class Status extends Component {
         this.setState({id: id});
 
         this.props.getVehiclesStatus(id);
+        this.interval = setInterval(() => {this.props.getVehiclesStatus(id); console.log("Update...")}, 60000);
+    }
+   
+    componentWillUnmount() {
+       clearInterval(this.interval);
+    }
+
+    componentWillReceiveProps(props){
+        const { vehicles } = props;
+        // console.log(vehicles);
+        const { id } = this.state;
+        if(vehicles[0].vehicleID === id){
+            // console.log("State status: "+this.state.status+" | status in props: "+vehicles[0].status);
+            if(this.state.status !== vehicles[0].status){
+                this.setState({status: vehicles[0].status});
+            }
+            
+        }
     }
 
     render() {
-        const {vehicles} = this.props;
-        if(vehicles){
-            console.log(vehicles);
-            return <span>connection: {vehicles.status}</span>
-             
-        }else{
-            return <span>connection: 0</span>
-        }
+        const { status } = this.state;
+        // console.log(status);
+        return <span>connection: {status}</span>
     }
 }
 
 Status.propTypes = {
     getVehiclesStatus: PropTypes.func.isRequired,
+    // vehicles: PropTypes.array.isRequired
 };
 
 export default connect((state) => {return {vehicles: state.vehicles.vehicles}},{getVehiclesStatus})(Status);
